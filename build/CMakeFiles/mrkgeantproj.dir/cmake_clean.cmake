988e42ab685c4983842289feef0b87d43a782e76
mjbales <matthew.bales@gmail.com>
2016-02-04 17:52:55 +0100

    Removed build directory

--- build/CMakeFiles/mrkgeantproj.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/mrkgeantproj"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/mrkgeantproj.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
